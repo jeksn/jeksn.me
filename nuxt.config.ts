045f1b1b5eab6097779f9a5b67df028c4a793591
Johan <johan@jeksn.me>
2024-07-03 13:56:15 +0200

    Add NuxtHub module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxthq/studio', "nuxt-feedme"],
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    '@nuxthq/studio',
+    "nuxt-feedme",
+    "@nuxthub/core"
+  ],
   content: {
     documentDriven: true
   }
