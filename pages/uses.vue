182a04dd8cafa936bdfd61ae082852c28c24ec58
Johan <johan@jeksn.me>
2024-09-26 22:10:19 +0200

    Update Uses

--- pages/uses.vue
+++ pages/uses.vue
@@ -22,20 +22,21 @@ definePageMeta({
                 <br>
                 <h2 class="text-2xl">Apps & Services</h2>
                 <ul class="mt-4 list-disc list-inside">
-                    <li>Arc</li>
+                    <li>Firefox (replaced Arc)</li>
                     <li>VS Code (and sometimes NeoVim)</li>
-                    <li>Warp</li>
+                    <li>Wezterm (replaced Warp)</li>
                     <li>Obsidian (for all my notes)</li>
                     <li>Proton Mail, Pass, and Drive</li>
                     <li>Feedbin (for RSS)</li>
                     <li>Airshow (for podcasts)</li>
                     <li>Raycast</li>
-                    <li>Apple Music</li>
+                    <li>Qobuz (replaced Apple Music)</li>
                 </ul>
                 <br>
                 <h2 class="text-2xl">Gear</h2>
                 <ul class="mt-4 list-disc list-inside">
                     <li>Macbook Air M1</li>
+                    <li>Lenovo Thinkpad X280 (running Linux Mint)</li>
                     <li>Keychron K3</li>
                     <li>Logitech MX Master 2S</li>
                     <li>Samsung T7 1TB SSD</li>
@@ -43,8 +44,8 @@ definePageMeta({
                 <br>
                 <h2 class="text-2xl">Headphones</h2>
                 <ul class="mt-4 list-disc list-inside">
-                    <li>Sony WH-XB900N</li>
                     <li>Soundcore P2 Mini</li>
+                    <li>Sony WH-XB900N</li>
                 </ul>
             </div>
         </div>
