8ab9d12dc05a1199d98e2e278c000d033afd7a75
Johan <johan@jeksn.me>
2024-09-27 10:17:55 +0200

    Change hero gradient

--- components/content/ContentHero.vue
+++ components/content/ContentHero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-screen bg-gradient-to-b from-neutral-800">
+    <div class="w-screen bg-gradient-to-b from-zinc-900">
         <div class="flex justify-center max-w-screen-lg px-4 py-20 mx-auto text-center text-white">
            <div>
                <slot />
