6054c24b1d180451d026f43402acefee5057bb2b
Johan <johan@jeksn.me>
2024-09-29 11:09:03 +0200

    Change design on Small projects page

--- pages/small-projects.vue
+++ pages/small-projects.vue
@@ -17,20 +17,56 @@ definePageMeta({
       <h1 class="mb-4 text-4xl">{{ title }}</h1>
     </Hero>
     <PageContent>
-      <ul class="flex mt-12 text-base md:text-lg">
-          <li class="flex items-center justify-center gap-4 py-4 border border-white rounded hover:bg-white hover:text-black w-fit px-9">
-              <p class="cursor-default">Week Number</p>
-          <span class="cursor-default">|</span>
-          <a href="https://weeek.xyz" target="_blank" class="transition-all ease-in-out">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=1.5 stroke="currentColor" class="w-6 h-6 transition ease-linear hover:scale-110">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-          </svg>
-          </a>
-          <a href="https://github.com/jeksn/weeknumber-nuxt" target="_blank" class="transition-all ease-in-out">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=1.5 stroke="currentColor" class="w-6 h-6 transition ease-linear hover:scale-110"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
-          </a>
-          </li>
-      </ul>
+      <div class="flex flex-col gap-6">
+        <div class="flex justify-between w-full mx-auto text-white rounded md:w-5/6 bg-neutral-950 hover:bg-neutral-900 border-[rgb(50,50,50)] border transition duration-150 ease-in-out">
+          <div class="w-1/2">
+            <a href="https://alpinist.jeksn.me" target="_blank">
+              <img src="/alpinist-screenshot.png" alt="" class="w-full h-[300px] object-cover object-top filter brightness-[20%] hover:brightness-100 transition duration-300 ease-in-out">
+            </a>
+          </div>
+          <div class="flex flex-col justify-between w-1/2 px-8 py-6">
+            <div class="space-y-3">
+              <h2 class="text-2xl">
+                Alpinist
+              </h2>
+              <p>A small collection of simple Alpine.js components and utilities.</p>
+            </div>
+              <div class="flex gap-x-4">
+                <a href="https://alpinist.jeksn.me" target="_blank" class="px-4 py-2 transition-all duration-100 ease-in-out border border-white rounded hover:bg-white hover:text-black">
+                    Go to site
+                  </a>
+                  <a href="#" target="_blank" class="px-4 py-2 transition-all duration-100 ease-in-out border border-white rounded hover:bg-white hover:text-black">
+                    Repo
+
+                  </a>
+                </div>
+          </div>
+        </div>
+        <div class="flex justify-between w-full mx-auto text-white rounded md:w-5/6 bg-neutral-950 hover:bg-neutral-900 border-[rgb(50,50,50)] border transition duration-150 ease-in-out">
+          <div class="w-1/2">
+            <a href="https://alpinist.jeksn.me" target="_blank">
+              <img src="/weeek-screenshot.png" alt="" class="w-full h-[300px] object-cover object-top filter brightness-[20%] hover:brightness-100 transition duration-300 ease-in-out">
+            </a>
+          </div>
+          <div class="flex flex-col justify-between w-1/2 px-8 py-6">
+            <div class="space-y-3">
+              <h2 class="text-2xl">
+                weeek.xyz
+              </h2>
+              <p>A small Nuxt site that shows the number of the week.</p>
+            </div>
+              <div class="flex gap-x-4">
+                <a href="https://weeek.xyz" target="_blank" class="px-4 py-2 transition-all duration-100 ease-in-out border border-white rounded hover:bg-white hover:text-black">
+                    Go to site
+                  </a>
+                  <a href="https://github.com/jeksn/weeknumber-nuxt" target="_blank" class="px-4 py-2 transition-all duration-100 ease-in-out border border-white rounded hover:bg-white hover:text-black">
+                    Repo
+
+                  </a>
+                </div>
+          </div>
+        </div>
+    </div>
     </PageContent>
   </NuxtLayout>
 </template>
