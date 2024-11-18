e73e40bee3a02af29166c52f6f9254411a0e79dd
Johan <johan@jeksn.me>
2024-11-18 21:12:19 +0100

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,24 +8,21 @@ definePageMeta({
     <NuxtLayout>
 		<PageContent>
 			<div class="flex flex-col items-center justify-start mt-[25vh] px-4">
-				<div class="flex flex-col items-start justify-center gap-6">
+				<div class="flex flex-col items-start justify-center">
 					<!-- <div class="flex items-start justify-center">
 						<NuxtImg src="/blue.png" class="transition-all duration-200 rounded-full size-24 hover:scale-110" />
 					</div> -->
 					<div class="flex flex-col text-lg gap-y-1 md:text-2xl">
-						<h1 class="my-3 text-4xl italic font-light">jeksn.me</h1>
+						<h1 class="my-3 text-3xl italic font-thin">jeksn.me</h1>
 						<p>
-							Design-minded developer in Stockholm.
+							Design-minded developer in Stockholm <span class="text-2xl">🇸🇪</span>
 						</p>
 						<p>
-							Currently working at <a href="https://wtmg.se" class="underline transition-all underline-offset-4 hover:underline-offset-8">WTMG.</a>
-						</p>
-						<p>
-							This is my small home on the web.
+							Currently working at <a href="https://wtmg.se" target="_blank" class="underline transition-all underline-offset-4 hover:underline-offset-8">WTMG</a>
 						</p>
 					</div>
 					<div class="mt-8">
-						<h2 class="text-lg md:text-xl">Where to find me:</h2>
+						<h2 class="text-lg md:text-xl">Where to find me</h2>
 						<ul class="mt-1 space-y-1 text-base">
 							<li>
 								<a
